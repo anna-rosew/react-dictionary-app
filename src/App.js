c45e77d498d79f9a5ae81381de6f8270b1766e76
Anna <annarosewain9@gmail.com>
2024-02-21 12:23:25 +0000

    Create Dictionary component with states for Search Engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="dictionary-app">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <small>Coded by Anna-Rose Wain</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyWord, setKeyWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyWord}`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyWord(event.target.value);
+  }
+
+  return (
+    <div className="dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
