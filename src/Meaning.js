3377d5eab66c2f24173a671cd79a5ab3a1127849
Anna <annarosewain9@gmail.com>
2024-02-22 10:57:26 +0000

    Meaning.js development

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,7 +4,17 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
+      {" "}
       <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+            <br />
+            <em>{definition.example}</em>
+          </div>
+        );
+      })}
       <p>{props.meaning.definitions[0].definition}</p>
     </div>
   );
