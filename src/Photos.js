078bbc85cbb88048d73f62c425f45f2fe7266d8c
Anna <annarosewain9@gmail.com>
2024-02-29 11:44:25 +0000

    Bootstrap error fixed

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function photos(props) {
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-2" key={index}>
+              <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.small}
